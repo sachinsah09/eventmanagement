2aea641b1f5cd2972606eb7b5fbb2b4b12d69ec3
sachinsah09 <ssa.axelor@gmail.com>
2019-09-16 17:51:33 +0530

    Add report for event

--- modules/event-management/src/main/java/com/axelor/event/web/EventController.java
+++ modules/event-management/src/main/java/com/axelor/event/web/EventController.java
@@ -2,13 +2,9 @@ package com.axelor.event.web;
 
 import com.axelor.event.db.Event;
 import com.axelor.event.service.EventService;
-import com.axelor.i18n.I18n;
-import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.inject.Inject;
-import com.axelor.app.AppSettings;
-import java.io.File;
 import java.lang.invoke.MethodHandles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,17 +25,5 @@ public class EventController {
 		} catch (Exception e) {
 			response.setError("Something went wrong ! please try again");
 		}
-	}
-
-	public void generateInvoiceReport(ActionRequest request, ActionResponse response) {
-		Event event = request.getContext().asType(Event.class);
-		String name = "Event";
-
-		String fileLink = ReportFactory.createReport(name + "-${date}")
-				.addParam("eventId", event.getId()).addParam("Locale", ReportSettings.getPrintingLocale(null))
-				.generate().getFileLink();
-
-		logger.debug("Printing " + name);
-		response.setView(ActionView.define(name).add("html", fileLink).map());
 	}
 }
